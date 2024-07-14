ac954a68e8965640362ed2612d591ea94d57f53a
anshroy2 <anshroytwo@gmail.com>
2024-07-14 14:49:52 -0500

    commenting out libraries, code that uses those libraries

--- api/index.py
+++ api/index.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
-from pprint import pprint
-from dotenv import load_dotenv
-import os
-import vonage
+# from pprint import pprint
+# from dotenv import load_dotenv
+# import os
+# import vonage
 
 app = Flask(__name__)
 
@@ -14,29 +14,29 @@ def home():
 def about():
     return 'About'
 
-@app.route('/send/', methods=['POST'])
-def send_sms():
-    pprint(request)
-    if request.is_json:
-        # send sms with whatever is in body
-        sms_body = request.get_json()
-        load_dotenv()
-        client = vonage.Client(key=os.environ['VONAGE_API_KEY'], secret=os.environ['VONAGE_API_SECRET'])
-        sms = vonage.Sms(client)
+# @app.route('/send/', methods=['POST'])
+# def send_sms():
+#     pprint(request)
+#     if request.is_json:
+#         # send sms with whatever is in body
+#         sms_body = request.get_json()
+#         load_dotenv()
+#         client = vonage.Client(key=os.environ['VONAGE_API_KEY'], secret=os.environ['VONAGE_API_SECRET'])
+#         sms = vonage.Sms(client)
 
-        responseData = sms.send_message(
-            {
-                "from": os.environ['SYSTEM_NUMBER'],
-                "to": os.environ['AJIT_NUMBER'],
-                "text": sms_body.get("text", "Hello from the other side."),
-            })
+#         responseData = sms.send_message(
+#             {
+#                 "from": os.environ['SYSTEM_NUMBER'],
+#                 "to": os.environ['AJIT_NUMBER'],
+#                 "text": sms_body.get("text", "Hello from the other side."),
+#             })
 
-        print (responseData)
-        if responseData["messages"][0]["status"] == "0":
-            print("Message sent successfully.")
-        else:
-            print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
-    return ('', 204)
+#         print (responseData)
+#         if responseData["messages"][0]["status"] == "0":
+#             print("Message sent successfully.")
+#         else:
+#             print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+#     return ('', 204)
 
 if __name__ == "__main__":
     app.run(debug=True)
