b213a7fbd764d00a936836b47eaafe08282d13a2
anshroy2 <anshroytwo@gmail.com>
2024-07-28 15:42:49 -0500

    try adding vonage sms

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ import vonage
 from urllib.parse import unquote_plus
 import json
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
@@ -41,26 +42,31 @@ def example_post_debug():
 @app.route('/send/', methods=['POST'])
 def send_sms():
     pprint(request)
-    if request.is_json:
-        # send sms with whatever is in body
-        sms_body = request.get_json()
-        load_dotenv()
-        client = vonage.Client(key=os.environ['VONAGE_API_KEY'], secret=os.environ['VONAGE_API_SECRET'])
-        sms = vonage.Sms(client)
+    if request.get_data():
+        data = request.get_data().decode('UTF-8')
+        if data:
+            payload = unquote_plus(data)
+            if payload:
+                event = json.loads(payload)
+                client = vonage.Client(key=os.environ['VONAGE_API_KEY'], secret=os.environ['VONAGE_API_SECRET'])
+                sms = vonage.Sms(client)
+                responseData = sms.send_message(
+                    {
+                        "from": os.environ['SYSTEM_NUMBER'],
+                        "to": os.environ['AJIT_NUMBER'],
+                        "text": event.get("text", "Default body Text Sent!"),
+                    })
 
-        responseData = sms.send_message(
-            {
-                "from": os.environ['SYSTEM_NUMBER'],
-                "to": os.environ['AJIT_NUMBER'],
-                "text": sms_body.get("text", "Hello from the other side."),
-            })
-
-        print (responseData)
-        if responseData["messages"][0]["status"] == "0":
-            print("Message sent successfully.")
+                print (responseData)
+                if responseData["messages"][0]["status"] == "0":
+                    print("Message sent successfully.")
+                else:
+                    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+                return ('Text cannot be accessed', 422)
+            return ('unquote did not work', 422)
         else:
-            print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
-    return ('', 204)
+            return ('Get DATA CANT DECODE', 422)
+    return ('Needs a body!', 422)
 
 if __name__ == "__main__":
     app.run(debug=True)
